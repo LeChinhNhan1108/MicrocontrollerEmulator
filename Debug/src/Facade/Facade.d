94c1bc82b0194f90af209c18fe3bfe0fa7a636be
root <root@db-concepts>
2014-07-13 16:58:31 +0700

    Delete files manually

--- Debug/src/Facade/Facade.d
+++
@@ -1,6 +0,0 @@
-src/Facade/Facade.d: ../src/Facade/Facade.cpp ../src/Facade/Facade.h \
- ../src/Facade/../Utilities/Utils.h
-
-../src/Facade/Facade.h:
-
-../src/Facade/../Utilities/Utils.h:
--- Debug/src/Utilities/Utils.d
+++
@@ -1,4 +0,0 @@
-src/Utilities/Utils.d: ../src/Utilities/Utils.cpp \
- ../src/Utilities/Utils.h
-
-../src/Utilities/Utils.h:
